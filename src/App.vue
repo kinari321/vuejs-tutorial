36a9195efd608f35f11da776590f9d07119c8d55
kinari321 <abcdream321@yahoo.co.jp>
2024-07-15 22:19:26 +0900

    宣言的レンダリング を記述

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
+<script setup>
+  import { reactive, ref } from 'vue';
+
+  const counter = reactive({
+    count: 0,
+  });
+
+  console.log(counter.count);
+  counter.count++;
+
+  const message = ref('Hello World!');
+
+  console.log(message.value);
+  message.value = 'Changed';
+</script>
+
 <template>
-  <h1>Hello World!</h1>
+  <h1>Make me dynamic!</h1>
+  <h1>{{ message }}</h1>
+  <h1>{{ message.split('').reverse().join('') }}</h1>
+  <p>Count is: {{ counter.count }}</p>
 </template>

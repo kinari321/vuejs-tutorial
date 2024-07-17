06d0c5f5eb98731d6e0b03df4423f7ba8bbb40f3
kinari321 <abcdream321@yahoo.co.jp>
2024-07-17 21:29:57 +0900

    イベントリスナーを記述

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <script setup>
   import { ref } from 'vue';
 
-  let colorClass = 'fuga';
-  const titleClass = ref(colorClass);
-  const dynamicId = ref('hoge');
+  const count = ref(0);
 
-  setTimeout(() => {
-    console.log('3秒経過しました');
-    colorClass = 'hoge';
-  }, 3000);
+  function increment() {
+    count.value++;
+  }
 </script>
 
 <template>
-  <h1 :class="titleClass">Make me red</h1>
-  <div :id="dynamicId">hogehoge</div>
+  <button @click="increment">Count is: {{ count }}</button>
+  <h1>Title</h1>
 </template>
-
-<style>
-  .fuga {
-    color: blue;
-  }
-</style>

9b600eea44a2150f1730a11fb0ec4e47fe2533c7
kinari321 <abcdream321@yahoo.co.jp>
2024-07-17 22:25:13 +0900

    リストレンダリングを記述

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <script setup>
   import { ref } from 'vue';
 
-  const awesome = ref(true);
+  // give each todo a unique id
+  let id = 0;
 
-  function toggle() {
-    awesome.value = !awesome.value;
+  const newTodo = ref('');
+  const todos = ref([
+    { id: id++, text: 'Learn HTML' },
+    { id: id++, text: 'Learn JavaScript' },
+    { id: id++, text: 'Learn Vue' },
+  ]);
+
+  function addTodo() {
+    todos.value.push({ id: id++, text: newTodo.value });
+    newTodo.value = '';
+  }
+
+  function removeTodo(clickedTodo) {
+    // todos.value = todos.value.filter(function (todo) {
+    //   return todo != clickedTodo;
+    // });
+    todos.value = todos.value.filter((todo) => todo != clickedTodo);
   }
 </script>
 
 <template>
-  <button @click="toggle">Toggle</button>
-  <h1 v-if="awesome">Vue is awesome!</h1>
-  <h1 v-else>Oh no 😢</h1>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo" />
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
